da4eb27efeaa72e4bd8674ee6f8320e716f8506b
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-03-27 10:36:50 +0200

    Add click action that toggles card bookmarked state

--- js/card-related.js
+++ js/card-related.js
@@ -1,5 +1,10 @@
 const cards = document.querySelectorAll('[data-js="card"]');
 
 cards.forEach((card) => {
-  console.log(card.querySelector('[data-js="card__bookmark-toggle-button"]'));
+  const bookmarkToggleButton = card.querySelector(
+    '[data-js="card__bookmark-toggle-button"]'
+  );
+  bookmarkToggleButton.addEventListener("click", () => {
+    bookmarkToggleButton.classList.toggle("card__bookmark-icon--selected");
+  });
 });
